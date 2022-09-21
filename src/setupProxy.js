8001980d719bea33c32de8708164d583953bcfc5
Michael Valdes <michael.j.valdes@gmail.com>
2022-09-21 17:53:43 -0400

    Run linting and prettier fixes on entire repo

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+import { createProxyMiddleware } from 'http-proxy-middleware'
 const proxy = {
-  target: "https://toxic-api-production.gggrunt16.workers.dev",
-  changeOrigin: true,
-};
-module.exports = function (app) {
-  app.use("/placement", createProxyMiddleware(proxy));
-  app.use("/stats", createProxyMiddleware(proxy));
-  app.use("/mmr", createProxyMiddleware(proxy));
-};
+    target: 'https://toxic-api-production.gggrunt16.workers.dev',
+    changeOrigin: true,
+}
+export default function (app) {
+    app.use('/placement', createProxyMiddleware(proxy))
+    app.use('/stats', createProxyMiddleware(proxy))
+    app.use('/mmr', createProxyMiddleware(proxy))
+}
